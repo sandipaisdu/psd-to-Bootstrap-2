8bb96f84096ac12a67fbb8c964279bb8afbb012e
Sandipaisdu <ksandipdu@gmail.com>
2015-06-23 03:40:49 +0600

    Second Assignment

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#nav').slicknav();
+
+
+    
+    $("#owl-demo").owlCarousel({
+
+        navigation: true,
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true
+
+        // "singleItem:true" is a shortcut for:
+        // items : 1, 
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
+
+
+});
